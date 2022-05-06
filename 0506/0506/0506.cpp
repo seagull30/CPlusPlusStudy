a687909993bca0d0ce828edae9d425eb31326597
seagull30 <minsang22@gmail.com>
2022-05-06 17:43:42 +0900

    0506

---
+++ 0506/0506/0506.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "headerfile.h"
+
+#define MIN(a,b) ((a) < (b) ? a : b)
+
+#define PRINT_ERROR(msg) printf("[%s:%d] %s\n", __FILE__, __LINE__, msg)
+int factorial(int a)
+{
+	
+}
+
+
+int main()
+{ 
+	int loopCount;
+	scanf("%d", &loopCount);
+	printf("%d", factorial(loopCount));
+
+	return 0;
+		
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//민상아 너무 어렵다 살려줘라 ㅜㅜㅜ
---
+++ 0506/0506/headerfile.h
@@ -0,0 +1,9 @@
+#pragma once
+
+
+typedef struct A
+{
+	int a;
+	int b;
+}A;
+int add(int a, int b);
