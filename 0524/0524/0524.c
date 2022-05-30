be5345ad3f2b2dacaed35eb945c99e7f7cc07da9
seagull30 <minsang22@gmail.com>
2022-05-30 18:04:46 +0900

    0530

---
+++ 0524/0524/0524.c
@@ -0,0 +1,62 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main()
+{
+#pragma region 백준2292벌집
+	//int inputNumber;
+	//scanf("%d", &inputNumber);
+	//
+	//int sum = 1;
+	//int count = 1;
+	//while (1)
+	//{
+	//	if (sum >= inputNumber)
+	//		break;
+	//	sum += 6 * count;
+	//	++count;
+	//}
+	//printf("%d", count);
+	//return 0;
+#pragma endregion
+
+
+#pragma region 백준1193분수찾기
+
+	//int inputNumber;
+	//scanf("%d", &inputNumber);
+	//
+	//int sum = 0;
+	//int count = 0;
+	//while (1)
+	//{
+	//	++count;
+	//	sum += count;
+	//	if (sum>=inputNumber)
+	//		break;
+	//}
+	//if (count % 2 == 0)
+	//{
+	//	printf("%d/%d", count - (sum - inputNumber), 1 + (sum - inputNumber));
+	//}
+	//else
+	//{
+	//	printf("%d/%d", 1 + (sum - inputNumber), count - (sum - inputNumber));
+	//}
+	//
+	//return 0;
+
+
+#pragma endregion 
+
+#pragma region 백준2869달팽이
+	//long long V, A, B;
+	//scanf("%lld %lld %lld", &A, &B, &V);
+	//printf("%lld", (V - B - 1) / (A - B) + 1);
+
+
+#pragma endregion
+
+
+
+}
