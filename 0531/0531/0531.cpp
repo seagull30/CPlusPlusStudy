88a2641d2ff53f2d0a7b7a97bc904123901b2be1
seagull30 <minsang22@gmail.com>
2022-05-31 17:50:27 +0900

    0531

---
+++ 0531/0531/0531.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include "Vector2D.h"
+/*
+using namespace std;
+
+int main()
+{
+	Vector2D vec(2, 4);
+	Vector2D vec2(3, 5);
+	
+	cout << vec + vec2 << '\n';
+	cout << vec - vec2 << '\n';
+	cout << vec * 2 << '\n';
+	cout << 2 * vec<< '\n';
+	/*
+	vec._x = 10;
+
+	// 1. 데이터 은닉에 위반 => 객체지향적 설계에 맞지 않음.
+	//	-데이터 은닉(Data Hidding) : 구현 세부 사항에 사용되는 데이터를 외부에 노출하지 않는 것
+	// 2. 디버깅이 어려워짐.
+	// 3. 확장성이 떨어짐.
+	
+}
+	*/
+
+
+class A
+{
+public:
+	int _data;
+	A& operator=(A a)
+	{
+		std::swap(_data, a._data);
+		return *this;
+	}
+};
+
+int main()
+{
+	A a;
+	a._data = 25;
+	A b;
+	b._data = 28;
+	int c = 23;
+	int d = 22;
+	std::swap(c, d);
+
+	std::cout << c << d << std::endl;
+	a = b;
+	std::cout << a._data << std::endl;
+	std::cout << b._data << std::endl;
+
+}
---
+++ 0531/0531/Vector2D.h
@@ -0,0 +1,49 @@
+﻿#pragma once
+
+#include <iostream>
+
+class Vector2D
+{
+public:
+	//생성자 : 필드의 초기화
+	Vector2D() : Vector2D(0, 0) {}
+	Vector2D(int x, int y) : _x(x), _y(y) {}
+
+	//접근자(Getter)
+	int GetX() const { return _x; }
+	int GetY() const { return _y; }
+	//설정자(setter)
+	void SetX(int x) { _x = x; }
+	void SetY(int y) { _y = y; }
+	// 두  벡터끼리 더함
+	Vector2D operator+(const Vector2D& rhs) const
+	{
+		return Vector2D(_x + rhs._x, _y + rhs._y);
+	}
+
+	// 두 벡터끼리 뺌
+	Vector2D operator-(const Vector2D& rhs) const
+	{
+		return Vector2D(_x - rhs._x, _y - rhs._y);
+	}
+
+	// 스칼라곱
+	Vector2D operator*(int rhs) const
+	{
+		return Vector2D(_x * rhs, _y * rhs);
+	}
+private:
+	int		_x;
+	int		_y;
+};
+
+Vector2D operator*(int lhs, const Vector2D& rhs)
+{
+	return rhs * lhs;
+}
+
+std::ostream& operator<<(std::ostream& oss, const Vector2D& vec)
+{
+	oss << "Vec(" << vec.GetX() << ", " << vec.GetY() << ")";
+		return oss;
+}
